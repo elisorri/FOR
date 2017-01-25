220dd3d2108b8b3b766b60daafb75423c4c98639
elisorri <elisos224@nemi.tskoli.is>
2017-01-25 13:36:45 +0000

    Liður 1

---
+++ Lidur_1.py
@@ -0,0 +1,8 @@
+#Elís Orri
+
+tala1 = int(input("Sláðu inn tölu "))
+tala2 = int(input("Sláðu inn aðra tölu "))
+summa = tala1 + tala2
+margfeldi = tala1 * tala2
+print(tala1, "+", tala2, "er sama sem", summa)
+print(tala1, "*", tala2, "er sama sem", margfeldi)
